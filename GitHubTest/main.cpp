1ff53bf64d0e81dcdca9f16e28c0a0f6bb608837
rvanderlely <101234963+rvanderlely@users.noreply.github.com>
2022-05-03 11:46:18 -0500

    I added a sentence to show grant how cool it is.

--- GitHubTest/main.cpp
+++ GitHubTest/main.cpp
@@ -14,5 +14,6 @@ int main()
 
    cout << "Hello, World. I'm using Github!\n";
     cout<<"This is very exciting.";
+    cout<<"I am showing grand the joys of xcode and github togetha.";
     return 0;
 }
